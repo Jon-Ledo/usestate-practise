4f0e68c0140eb5fdda17735d7af2130c7aba2c36
JonLedo <jon.ledo@hotmail.com>
2022-04-29 13:53:50 -0400

    simple clear all and rendering with state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 
 function App() {
+  const [value, setValue] = useState(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{value.length} birthdays today</h3>
+        <List people={value}/>
+        <button type="button" onClick={() => setValue([])}>Clear All</button>
+      </section>
+    </main>
   );
 }
 
